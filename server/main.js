e3dc2b4ba0ef2348644572fe2d3480374922c943
Mike-Albi-MK <mike.albrecht@dci-student.org>
2024-10-10 14:31:34 +0200

    code changes and enhancements

--- server/main.js
+++ server/main.js
@@ -1,14 +1,39 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 
 import connectToDB from "./libs/db.js";
+import userRoutes from "./routes/usersRouter.js";
+import projectRoutes from "./routes/projectsRouter.js";
+import { globalErrorHandler, routeNotFound } from "./middleware/errorHandler.js";
 
+//! Connection to database
 await connectToDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express());
-app.use(cors());
+//! Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//! CORS 
+app.use(
+    cors({
+      origin: process.env.EXPRESS_CLIENT_URL,
+      credentials: true,
+    })
+  );
+
+//! Routes
+app.use("api/users", userRoutes);
+app.use("/api/projects", projectRoutes);
+
+//! Catch 404 for undefined routes
+app.use(routeNotFound);
+//! Global error handler middleware
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
